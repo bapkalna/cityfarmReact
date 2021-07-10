5687935c14614d445c26705533ff0de5e932c2ca
Baiba Apkalna <apkalna@gmail.com>
2021-07-10 10:54:55 +0300

    Footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,35 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, FormGroup, Label, Input, Col } from "reactstrap";
 
 function Footer(props) {
-    return (
-        <footer className="site-footer">
-            <div className="container">
-                <div className="row">             
-                    <div className="col-4 col-sm-2 offset-1">
-                        <h5>Links</h5>
-                        <ul className="list-unstyled">
-                            <li><Link to='/home'>Home</Link></li>
-                            <li><Link to='/directory'>Directory</Link></li>
-                            <li><Link to='/aboutus'>About</Link></li>
-                            <li><Link to='/contactus'>Contact</Link></li>
-                        </ul>
-                    </div>
-                    <div className="col-6 col-sm-3 text-center">
-                        <h5>Social</h5>
-                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
-                    </div>
-                    <div className="col-sm-4 text-center">
-                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
-                    </div>
+  return (
+    <footer className="site-footer text-center">
+      <div className="container">
+        <div className="row d-flex justify-content-center mb-4">
+          <div className="col-6 col-sm-3">
+            <a
+              className="btn btn-social-icon buttonStyle m-1"
+              href="http://instagram.com/"
+            >
+              <i className="fa fa-instagram" />
+            </a>{" "}
+            <a
+              className="btn btn-social-icon buttonStyle m-1"
+              href="http://www.facebook.com/"
+            >
+              <i className="fa fa-facebook" />
+            </a>{" "}
+            <a
+              className="btn btn-social-icon buttonStyle m-1"
+              href="http://twitter.com/"
+            >
+              <i className="fa fa-twitter" />
+            </a>{" "}
+            <a
+              className="btn btn-social-icon buttonStyle m-1"
+              href="http://youtube.com/"
+            >
+              <i className="fa fa-youtube" />
+            </a>
+          </div>
+        </div>
+        <div>
+          <Form>
+            <div className="row d-flex justify-content-center">
+              <div className="col-auto">
+                <p className="pt-2">
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </div>
+              <div className="col-md-5 col-12">
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="subscribeNewsletter"
+                    className="form-control"
+                  />
+                  <label className="form-label" for="subscribeNewsletter">
+                    Email address
+                  </label>
                 </div>
+              </div>
+              <div className="col-auto">
+                <button type="submit" class="btn buttonStyle mb-4">
+                  Subscribe
+                </button>
+              </div>
             </div>
-        </footer>
-    );
+          </Form>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,55 +1,67 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  CardGroup,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardDeck,
+} from "reactstrap";
 
 function RenderBlogPost({ item }) {
   return (
-    <div className="row">
-      <h2>{item.name}</h2>
-      <p>{item.description}</p>
-      <div className="row">
-        <img src={item.image}></img>
+    <div className="row blogpost">
+      <div className="col-md-7">
+        <h2 className="blogpost-heading">{item.name}</h2>
+        <p>{item.description}</p>
+      </div>
+      <div className="col-md-5">
+        <img className="blogpost-image mx-auto" src={item.image}></img>
       </div>
     </div>
-  );
-}
-
-function RenderCard({ item }) {
-  return (
-    <Card>
-      <CardImg src={item.image} alt={item.name} />
-      <CardBody>
-        <CardTitle>{item.name}</CardTitle>
-        <CardText>{item.description}</CardText>
-      </CardBody>
-    </Card>
   );
 }
 
 function Home(props) {
   return (
     <React.Fragment>
+      <hr className="blogpost-divider"></hr>
       <div className="container">
-          <RenderBlogPost item={props.garden} />
+        <RenderBlogPost item={props.garden} />
       </div>
+      <hr className="blogpost-divider"></hr>
       <div className="container">
-          <RenderBlogPost item={props.animal} />
+        <RenderBlogPost item={props.animal} />
       </div>
+      <hr className="blogpost-divider"></hr>
       <div className="container">
-          <RenderBlogPost item={props.bee} />
+        <RenderBlogPost item={props.bee} />
       </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md m-1">
-            <RenderCard item={props.garden} />
-          </div>
-          <div className="col-md m-1">
-            <RenderCard item={props.bee} />
-          </div>
-          <div className="col-md m-1">
-            <RenderCard item={props.animal} />
-          </div>
-        </div>
-      </div>
+      <hr className="blogpost-divider"></hr>
+      <CardDeck>
+      <Card className="card-group">
+          <CardImg className="card-img-top" src="assets/images/chickens3.jpeg"  />
+          <CardBody>
+            <CardTitle>ANIMALS</CardTitle>
+            <CardText>The best source of information about animals in the urban backyards.</CardText>
+          </CardBody>
+        </Card>
+        <Card className="card-group">
+          <CardImg className="card-img-top" src="assets/images/garden2.jpeg" />
+          <CardBody>
+            <CardTitle>GARDENS</CardTitle>
+            <CardText>Gardening knowledge, tips and tricks, experiences and all the good stuff.</CardText>
+          </CardBody>
+        </Card>
+        <Card className="card-group">
+          <CardImg className="card-img-top" src="assets/images/bees.jpeg"  />
+          <CardBody>
+            <CardTitle>BEES</CardTitle>
+            <CardText>The best source to find information on beekeeping in the urban areas.</CardText>
+          </CardBody>
+        </Card>
+      </CardDeck>
     </React.Fragment>
   );
 }
